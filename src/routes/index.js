462b8aa0aba2f86645411bb8099bf610e677019f
Kelvin Hao <huynhnhathao0609@gmail.com>
2024-07-11 21:51:48 +0700

    Add route check health of server

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,10 @@ const express = require('express')
 
 const route = express.Router()
 
+route.get('/health-server', (req, res) => {
+	res.status(200).json({ status: 'OK', message: 'Server is healthy' })
+})
+
 route.use('/user', require('./user.route'))
 route.use('/APIKey', require('./APIKey.route'))
 route.use('/template', require('./template.route'))
